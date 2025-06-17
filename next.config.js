966d5374eb24311e7203cbcefa67ad6ac483b9bc
Egor <egor@mindstream.at>
2025-06-17 16:40:54 +0200

    use next-image

--- next.config.js
+++ next.config.js
@@ -2,10 +2,17 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -7,10 +8,16 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Image
+            src={image.url}
+            alt={image.name}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+          />
           <div className="text-sm">{image.name}</div>
         </div>
       ))}
